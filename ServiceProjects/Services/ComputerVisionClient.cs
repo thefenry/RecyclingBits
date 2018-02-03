3a8e45c8a3ea035719859593eb7edf0e6190fbed
Ivan Sued <thefenry@gmail.com>
2018-02-03 09:11:20 -0800

    Adding packages and service for the Computer vision client

---
+++ ServiceProjects/Services/ComputerVisionClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Cognitive.CustomVision.Prediction;
+using ServiceProjects.Interfaces;
+
+namespace ServiceProjects.Services
+{
+    class ComputerVisionClient: IComputerVisionClient
+    {
+        public ComputerVisionClient()
+        {
+            // Create a prediction endpoint, passing in the obtained prediction key
+            //PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = predictionKey };
+            //var testImage = new object();
+            //// Make a prediction against the new project
+            ////Console.WriteLine("Making a prediction:");
+            ////var result = endpoint.PredictImage(project.Id, testImage);
+            //endpoint.PredictImageUrlWithHttpMessagesAsync()
+            //ServiceKey
+        }
+
+    }
+}
