49ec81522359f2aa5832afd00ed104ff0587d793
yichiang <yi_chiang@outlook.com>
2018-02-03 09:12:15 -0800

    Set up APIs

---
+++ RecyclingAPI/Controllers/RecyclingController.cs
@@ -0,0 +1,29 @@
+﻿using ServiceProjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace RecyclingAPI.Controllers
+{
+    [EnableCors(origins:"*", headers: "*", methods: "*")]
+    public class RecyclingController : ApiController
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpPost]
+        [ActionName("GetPrediction")]
+        public async Task<string> GetPredictionAsync([FromBody]byte[] value)
+        {
+            return await CSPrediction.MakePredictionRequest(value);
+        }
+    }
+}
---
+++ ServiceProjects/CSPrediction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceProjects
+{
+    public static class CSPrediction
+    {
+        private static string _key { get; } = "d8fab3898adc430c80ad1669c875c4ff";
+        private static string _url { get; } = "http://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/prediction/d16e136c-5b0b-4b84-9341-6a3fff8fa7fe/image?iterationId=f4e573f6-9843-46db-8018-b01d034fd0f2";
+
+        //static byte[] GetImageAsByteArray(string imageFilePath)
+        //{
+        //    FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+        //    BinaryReader binaryReader = new BinaryReader(fileStream);
+        //    return binaryReader.ReadBytes((int)fileStream.Length);
+        //}
+
+        public static async Task<string> MakePredictionRequest(byte[] byteData)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Prediction-Key", _key);
+
+
+            HttpResponseMessage response;
+
+            //byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(_url, content);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
